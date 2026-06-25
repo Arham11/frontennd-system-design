ca49be142e364bee94a3d259244d7264896fc628
arham-chowdhry_ABT <arham.chowdhry@abbott.com>
2026-06-25 13:15:12 +0530

    Shimmer UI

---
+++ LLD/src/app/shimmer/shimmer.tsx
@@ -0,0 +1,10 @@
+function Shimmer() {
+  return (
+    <div className="p-3 h-full border border-gray-200 rounded-lg">
+      <p className="h-6 bg-gray-200 mb-2.5 w-3/4"></p>
+      <div className="bg-gray-200 h-50" />
+    </div>
+  );
+}
+
+export default Shimmer;
